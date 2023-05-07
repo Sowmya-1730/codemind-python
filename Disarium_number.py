f95bd57c57f387948df9466aff1df124af935fc8
Sowmya <119840097+Sowmya-1730@users.noreply.github.com>
2023-05-07 10:07:14 +0530

    n=int(input()) l=0 q=n q1=n while n!=0:     r=n%10     l=l+1     n=n//10 s=0 while q!=0:     r=q%10     s+=pow(r,l)     l=l-1     q=q//10 if s==q1:     print("True") else:     print("False")

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,20 +1,18 @@
-import math
 n=int(input())
 l=0
-sm=0
-nn=n
 q=n
+q1=n
 while n!=0:
     r=n%10
-    l+=1
+    l=l+1
     n=n//10
+s=0
 while q!=0:
     r=q%10
-    s=pow(r,l)
-    sm+=s
+    s+=pow(r,l)
     l=l-1
     q=q//10
-if sm==nn:
+if s==q1:
     print("True")
 else:
     print("False")
