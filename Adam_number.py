76650646f76c78baac82c3df7d1b683d7f5bddbd
Sowmya <119840097+Sowmya-1730@users.noreply.github.com>
2023-05-07 09:58:58 +0530

    n=int(input()) ns=n*n s=0 s1=0 while n!=0:     r=n%10     s=s*10+r     n=n//10 rs=s*s while rs!=0:     r=rs%10     s1=s1*10+r     rs=rs//10 if ns==s1:     print("True") else:     print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -6,13 +6,13 @@ while n!=0:
     r=n%10
     s=s*10+r
     n=n//10
-nr=s*s
-nrs=nr
-while nr!=0:
-    r=nr%10
+rs=s*s
+while rs!=0:
+    r=rs%10
     s1=s1*10+r
-    nr=nr//10
+    rs=rs//10
 if ns==s1:
     print("True")
 else:
     print("False")
+    
