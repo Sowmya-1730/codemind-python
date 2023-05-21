62c2bd3513184238287f5078dc0018ae7a86257c
Sowmya <119840097+Sowmya-1730@users.noreply.github.com>
2023-05-21 10:48:13 +0530

    n=int(input()) l=0 q=n lst=[] while n>0:     r=n%10     l+=1     n=n//10 while q>0:     r1=q%10     if r1 not in lst:         lst.append(r1)     else:         lst.remove(r1)     q=q//10 if len(lst)==l:     print('Unique Number') else:     print('Not Unique Number')

--- Unique_number.py
+++ Unique_number.py
@@ -1,13 +1,19 @@
 n=int(input())
+l=0
+q=n
 lst=[]
-sm=0
 while n>0:
     r=n%10
-    sm+=1
-    if r in lst:
-        print("Not Unique Number")
-        break
-    lst.append(r)
+    l+=1
     n=n//10
-if len(lst)==sm:
-    print("Unique Number")
+while q>0:
+    r1=q%10
+    if r1 not in lst:
+        lst.append(r1)
+    else:
+        lst.remove(r1)
+    q=q//10
+if len(lst)==l:
+    print('Unique Number')
+else:
+    print('Not Unique Number')
